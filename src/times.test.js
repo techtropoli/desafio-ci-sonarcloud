3308f8ffcd485ad8fc5788212282e986da90ca42
Gabriele Almeida <naogabe@gmail.com>
2023-10-16 16:29:53 -0300

    make a pull request not pass the quality gate

--- src/times.test.js
+++ src/times.test.js
@@ -1,5 +1,5 @@
 const times = require('./times');
 
-test("times: 2 * 3 to be equal 6", () => {
-    expect(times(2,3)).toBe(6);
-});
+// test("times: 2 * 3 to be equal 6", () => {
+//     expect(times(2,3)).toBe(6);
+// });
